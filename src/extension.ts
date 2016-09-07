26184ada7bed3ac7bb6d0db98dc667ebed7e78c8
Philipp Kief <Philipp Kief>
2016-09-07 23:41:47 +0200

    fixed bug

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-var async = require("async");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
