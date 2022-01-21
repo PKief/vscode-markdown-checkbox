2999cee9100e53be708b006340294e1d458ea70f
Philipp Kief <philipp.kief@gmx.de>
2022-01-21 22:24:44 +0100

    Implement Toggle create checkbox

--- src/createCheckbox.ts
+++ src/createCheckbox.ts
@@ -1,29 +1,44 @@
-import { Position, TextEditor, TextEditorEdit, TextLine } from 'vscode';
-import * as helpers from './helpers';
+import { Position, Range, TextEditor, TextEditorEdit, TextLine } from "vscode";
+import * as helpers from "./helpers";
 
 /** Create a new checkbox at selected lines or the current cursor position */
 export const createCheckbox = async (editor: TextEditor) => {
-    const selection = editor.selection;
+  const selection = editor.selection;
 
-    for (let r = selection.start.line; r <= selection.end.line; r++) {
-        const line = editor.document.lineAt(r);
-        await createCheckboxOfLine(editor, line);
-    }
+  for (let r = selection.start.line; r <= selection.end.line; r++) {
+    const line = editor.document.lineAt(r);
+    await createCheckboxOfLine(editor, line);
+  }
 };
 
-const createCheckboxOfLine = (editor: TextEditor, line: TextLine): Thenable<boolean> => {
-    const withBulletPoint = helpers.getConfig<boolean>('withBulletPoint');
-    const typeOfBulletPoint = helpers.getConfig<string>('typeOfBulletPoint');
-    const hasBullet = helpers.lineHasBulletPointAlready(line);
+const createCheckboxOfLine = (
+  editor: TextEditor,
+  line: TextLine
+): Thenable<boolean> => {
+  const withBulletPoint = helpers.getConfig<boolean>("withBulletPoint");
+  const typeOfBulletPoint = helpers.getConfig<string>("typeOfBulletPoint");
+  const hasBullet = helpers.lineHasBulletPointAlready(line);
 
-    if (!helpers.getCheckboxOfLine(line)) {
-        return editor.edit((editBuilder: TextEditorEdit) => {
-            editBuilder.insert(new Position(
-                line.lineNumber,
-                hasBullet.pos
-            ), (withBulletPoint && !hasBullet.bullet ? typeOfBulletPoint + ' ' : '') + '[ ] ');
-        });
+  const checkboxOfLine = helpers.getCheckboxOfLine(line);
+  const checkboxCharacters = "[ ] ";
+
+  return editor.edit((editBuilder: TextEditorEdit) => {
+    if (!checkboxOfLine) {
+      editBuilder.insert(
+        new Position(line.lineNumber, hasBullet.pos),
+        (withBulletPoint && hasBullet.bullet ? "" : typeOfBulletPoint + " ") +
+          checkboxCharacters
+      );
     } else {
-        return Promise.resolve(undefined);
+      editBuilder.delete(
+        new Range(
+          new Position(line.lineNumber, hasBullet.pos),
+          new Position(
+            line.lineNumber,
+            hasBullet.pos + checkboxCharacters.length
+          )
+        )
+      );
     }
+  });
 };
