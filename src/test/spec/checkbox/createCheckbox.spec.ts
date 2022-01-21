9ce9c7818c81dd708ba0168e6d1351a3c80e381d
ryotatake <39555429+ryotatake@users.noreply.github.com>
2022-01-22 04:30:51 +0900

    Add support for creating checkboxes on selected lines (#31)
    * Content should be tested.
    
    * Add support for creating checkboxes on selected lines.

--- src/test/spec/checkbox/createCheckbox.spec.ts
+++ src/test/spec/checkbox/createCheckbox.spec.ts
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { createCheckbox } from '../../../createCheckbox';
-import { getEditor } from '../../../helpers';
+import { getEditor, getConfig } from '../../../helpers';
 import { useDefaultSettings } from '..';
 
 describe('create checkboxes', () => {
@@ -24,9 +24,13 @@ describe('create checkboxes', () => {
         const newSelection = new vscode.Selection(newCursorPosition, newCursorPosition);
         editor.selection = newSelection;
 
-        const result = await createCheckbox(editor);
+        await createCheckbox(editor);
 
-        assert.equal(result, true);
+        const content = editor.document.getText();
+        const typeOfBulletPoint = getConfig<string>('typeOfBulletPoint');
+        const expectedResult = `${typeOfBulletPoint} [ ] this is a text`;
+
+        assert.equal(content, expectedResult);
     });
 
     it('should be created without new bullet point', async () => {
@@ -44,8 +48,35 @@ describe('create checkboxes', () => {
         const newSelection = new vscode.Selection(newCursorPosition, newCursorPosition);
         editor.selection = newSelection;
 
-        const result = await createCheckbox(editor);
+        await createCheckbox(editor);
 
-        assert.equal(result, true);
+        const content = editor.document.getText();
+        const expectedResult = `- [ ] this is a text`;
+
+        assert.equal(content, expectedResult);
+    });
+
+    it('should be created with new bullet points', async () => {
+        // create new document
+        const newDocument = await vscode.workspace.openTextDocument({
+            content: 'this is a text\nthis is a second text\n- this is a third text',
+            language: 'markdown'
+        });
+        await vscode.window.showTextDocument(newDocument);
+
+        // create a selection over the text
+        const editor = getEditor();
+        const startPosition = new vscode.Position(0, 0);
+        const endPosition = new vscode.Position(2, 0);
+        const newSelection = new vscode.Selection(startPosition, endPosition);
+        editor.selection = newSelection;
+
+        await createCheckbox(editor);
+
+        const content = editor.document.getText();
+        const typeOfBulletPoint = getConfig<string>('typeOfBulletPoint');
+        const expectedResult = `${typeOfBulletPoint} [ ] this is a text\n${typeOfBulletPoint} [ ] this is a second text\n- [ ] this is a third text`
+
+        assert.equal(content, expectedResult);
     });
 });
